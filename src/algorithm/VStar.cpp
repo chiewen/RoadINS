645e8ab93f93e6e232b3d9b73878ecf4cb463866
Chiewen <lichuanwen@ise.neu.edu.cn>
2015-12-04 09:46:02 +0800

    minor changes

--- src/algorithm/VStar.cpp
+++ src/algorithm/VStar.cpp
@@ -2,7 +2,6 @@
 // Created by chiewen on 2015/11/24.
 //
 
-#include <iostream>
 #include "VStar.h"
 #include "Dijkstra.h"
 
@@ -20,7 +19,6 @@ void VStar::move(Trajectory trajectory) {
 }
 
 void VStar::calculate(const pair<PRoad, double> &pos) {
-    cout << "!!!!" << endl;
     Dijkstra::top_k_vstar(pos.first->to.lock(), pos.second, k + x, ordered_k_x);
     q_b = pos;
     z = ordered_k_x[k + x - 1];
@@ -28,38 +26,21 @@ void VStar::calculate(const pair<PRoad, double> &pos) {
 }
 
 bool VStar::validate_knn(const pair<PRoad, double> &pos) {
-    cout << "new pos" << endl;
-    //refresh Fixed Rank Region (FRR)
-    long from_id = pos.first->from.lock()->id;
     long to_id = pos.first->to.lock()->id;
-
-    typedef typename FrrMap::mapped_type FrrKX;
-
-    auto pFrom = frrMap.find(from_id);
-    if (pFrom == frrMap.end()) {
-        FrrKX topk;
-        Dijkstra::top_k_with_distance(pos.first->from.lock(), k + x, ordered_k_x, topk);
-        pFrom = frrMap.insert(make_pair(from_id, topk)).first;
-    }
-
     auto pTo = frrMap.find(to_id);
     if (pTo == frrMap.end()) {
-        FrrKX topk;
+        map<long, double> topk;
         Dijkstra::top_k_with_distance(pos.first->to.lock(), k + x, ordered_k_x, topk);
         pTo = frrMap.insert(make_pair(to_id, topk)).first;
     }
 
     bool should_recalculate = true;
-    while(should_recalculate) {
+    while (should_recalculate) {
         should_recalculate = false;
         for (int i = 0; i < k + x - 1; i++)
-            if (min(pFrom->second[ordered_k_x[i]->id] + pos.first->distance - pos.second,
-                    pTo->second[ordered_k_x[i]->id] + pos.second) >
-                min(pFrom->second[ordered_k_x[i + 1]->id] + pos.first->distance - pos.second,
-                    pTo->second[ordered_k_x[i + 1]->id] + pos.second)) {
+            if (pTo->second[ordered_k_x[i]->id] + pos.second > pTo->second[ordered_k_x[i + 1]->id] + pos.second) {
                 swap(ordered_k_x[i], ordered_k_x[i + 1]);
                 should_recalculate = true;
-                cout << i << " : " << ordered_k_x[i]->id << " : " << ordered_k_x[i+1]->id <<endl;
             }
     }
 
@@ -68,7 +49,7 @@ bool VStar::validate_knn(const pair<PRoad, double> &pos) {
         z_dist = Dijkstra::distance_to(q_b.first->to.lock(), q_b.second, z);
     }
 
-    //validate the safe region with regard to the k'th nearest neighbor
     return z_dist > Dijkstra::distance_to(pos.first->to.lock(), pos.second, ordered_k_x[k - 1]) +
-                    Dijkstra::distance_to(pos.first->to.lock(), pos.second + q_b.second, q_b.first->to.lock());
+                    Dijkstra::distance_to(pos.first->to.lock(), pos.second + q_b.first->distance - q_b.second,
+                                          q_b.first->from.lock());
 }
