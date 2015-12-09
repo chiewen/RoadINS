a49e137bb26fa9e0b8dd9e3e1e8560f869a6fc9b
Chuanwen Li <lichuanwen@ise.neu.edu.cn>
2015-12-09 22:20:06 +0800

    before adding R objects

--- test/experiment.cpp
+++ test/experiment.cpp
@@ -5,8 +5,6 @@
 
 #include <gtest/gtest.h>
 #include <thread>
-#include <functional>
-#include <algorithm>
 #include "../src/network/Node.h"
 #include "../src/network/Trajectory.h"
 #include "../src/algorithm/INS.h"
@@ -21,7 +19,7 @@ using namespace std::placeholders;
 typedef typename vector<shared_ptr<MknnProcessor>>::iterator MPIter;
 
 void print_result(string name, MPIter start, MPIter end) {
-    cout << name <<":\t"
+    cout << name << ":\t"
     << accumulate(start, end, 0,
                   [](long s, const shared_ptr<MknnProcessor> &ptr) { return s + ptr->num_page(); }) / 5 << "\t"
     << accumulate(start, end, 0,
@@ -72,36 +70,27 @@ void execute(const vector<PNode> &nodes, vector<Trajectory> &trajectories, int k
     for (auto &t: trajectories)
         t.setStep(step);
 
-    vector<shared_ptr<MknnProcessor>> vs;
-    vs.reserve(10);
-    for (int i = 0; i < 10; ++i)
-        vs.emplace_back(make_shared<VStar>(k, x));
-
-    vector<shared_ptr<MknnProcessor>> ins;
-    ins.reserve(10);
-    for (int i = 0; i < 10; ++i)
-        ins.emplace_back(make_shared<INS>(k));
+    vector<shared_ptr<MknnProcessor>> mps;
+    mps.reserve(24);
+    for (int i = 0; i < 12; ++i)
+        mps.emplace_back(make_shared<VStar>(k, x));
+    for (int i = 0; i < 12; ++i)
+        mps.emplace_back(make_shared<INS>(k));
 
     vector<thread> vt;
-    for (int i = 0; i < 10; ++i)
-        vt.emplace_back([&vs, i, &trajectories]() {
-            vs[i]->move(trajectories[i]);
-        });
-
-    vt.clear();
-
-    for (int i = 0; i < 10; ++i)
-        vt.emplace_back([&ins, i, &trajectories]() {
-            ins[i]->move(trajectories[i]);
+    vt.reserve(24);
+    for (int i = 0; i < 24; ++i)
+        vt.emplace_back([&mps, i, &trajectories]() {
+            mps[i]->move(trajectories[i]);
         });
 
     for (auto &t : vt)
         t.join();
 
-    print_result("VR", vs.begin(), vs.begin() + 5);
-    print_result("VD", vs.begin() + 5, vs.end());
-    print_result("IR", ins.begin(), ins.begin() + 5);
-    print_result("ID", ins.begin() + 5, ins.end());
+    print_result("VR", mps.begin(), mps.begin() + 6);
+    print_result("VD", mps.begin() + 6, mps.begin() + 12);
+    print_result("IR", mps.begin() + 12, mps.begin() + 18);
+    print_result("ID", mps.begin() + 18, mps.end());
     cout << endl;
 }
 
@@ -114,16 +103,16 @@ TEST(Experiment, First) {
     int step = 100;
 
     vector<Trajectory> trajectories;
-    trajectories.reserve(10);
+    trajectories.reserve(24);
     cout << "\n============= experiment start at " << TimePrinter::now << " =============\n" << endl;
-    for (int i = 0; i < 100000; i += 20000)
+    for (int i = 0; i < 120000; i += 20000)
         trajectories.emplace_back(TrajectoryConstructor::construct_random(nodes[i], length, step));
 
     cout << "R " << TimePrinter::now << endl;
 
     vector<thread> vt;
     mutex m_traj;
-    for (int i = 0; i < 100000; i += 20000)
+    for (int i = 0; i < 120000; i += 20000)
         vt.emplace_back([i, length, step, &m_traj, &trajectories, &nodes]() {
             auto t = TrajectoryConstructor::construct_shortest_path(nodes[i], length, step);
             lock_guard<mutex> {m_traj};
@@ -134,6 +123,8 @@ TEST(Experiment, First) {
         t.join();
 
     cout << "D " << TimePrinter::now << endl;
+
+    copy(trajectories.begin(), trajectories.begin() + 12, back_inserter(trajectories));
 
 //    cout << endl << "k = 1 to 20" << endl;
 //    for (int i = 1; i < 20; i++)
